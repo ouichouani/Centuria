56974fd11b7c12aa2777edd264bfc2827d9ad442
ouichouani <ouichouaniabdlhakim@gmail.com>
2026-05-21 23:39:27 +0100

    fetch data into explore component

--- frontend_Centuria/app/main/explore/page.jsx
+++ frontend_Centuria/app/main/explore/page.jsx
@@ -1,5 +1,46 @@
+"use client";
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from '../../layout.tsx';
+import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 
-export default function explore(){
-    return <h1>explore</h1>
+export default function Explore() {
+
+    const { domain } = useContext(AppContext);
+    const [data, setData] = useState();
+    const router = useRouter();
+
+    useEffect(() => {
+        (async () => {
+            try {
+
+                const response = await fetch(`${domain}/posts`, {
+                    method: "GET",
+                    credentials: "include", // store the cookie from the response
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json"
+                    },
+                });
+
+                const result = await response.json();
+                console.log(result);
+
+                if (response.ok) setData(result.data);
+            } catch (error) {
+                console.error('error from console : ' + error);
+            }
+        })()
+        }, []);
+
+    return (
+        <>
+            {data?.map((item , key) =>
+                <h2> - {item.content}</h2>
+            )}
+
+        </>
+    )
 }
