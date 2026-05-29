3024f1a8912b2c06cd90b21fd484302d070456e0
ouichouani <ouichouaniabdlhakim@gmail.com>
2026-05-29 18:14:19 +0100

    working on board component , make the board component a server component and create HabitRow server component that show the <td> with the propieate data , and minimize the role of log client component to only create and delete logs

--- frontend_Centuria/app/main/dashboard/board/Log.jsx
+++ frontend_Centuria/app/main/dashboard/board/Log.jsx
@@ -1,8 +1,8 @@
-"use client" ;
+"use client";
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from '@/context/AppContext.jsx'
 
-export default function Log({ habit , index , key}) {
+export default function Log({ habit, index_date, current_log_index }) {
 
     const { domain } = useContext(AppContext);
     const [data, setData] = useState();
@@ -57,9 +57,9 @@ export default function Log({ habit , index , key}) {
 
     return (
         <>
-            {new Date(habit.last_log?.completed_date).toLocaleDateString() != index.toLocaleDateString() ?
+            {new Date(habit.last_log?.completed_date).setHours(0,0,0,0) != index_date ?
 
-                <td key={key} className="border-x border-solid border-[#86878b] min-w-[25px] text-center cursor-pointer">
+                <td className="border-x border-solid border-[#86878b] min-w-[25px] text-center cursor-pointer">
                     <form onSubmit={StoreLog}>
                         <input type="hidden" name="task_id" value={habit.id} />
                         <button className="cursor-pointer">
@@ -68,7 +68,7 @@ export default function Log({ habit , index , key}) {
                     </form>
                 </td>
                 :
-                <td key={key} className="border-x border-solid border-[#86878b] min-w-[25px] text-center bg-green-800 cursor-pointer">
+                <td className="border-x border-solid border-[#86878b] min-w-[25px] text-center bg-green-800 cursor-pointer">
                     <form onSubmit={DeleteLog}>
                         <input type="hidden" name="log_id" value={habit.last_log.id} />
                         <button className="cursor-pointer">
--- frontend_Centuria/app/main/dashboard/board/page.jsx
+++ frontend_Centuria/app/main/dashboard/board/page.jsx
@@ -1,53 +1,34 @@
-"use client";
-
-import { useContext, useEffect, useState } from "react";
-import { AppContext } from '@/context/AppContext.jsx'
-import Log from "./Log";
+import HabitRow from "./HabitRow";
+import { cookies } from "next/headers";
 
 
-export default function board() {
-    const { domain } = useContext(AppContext);
-    const [data, setData] = useState();
+export default async function board() {
+
+    // get domain from .env.local
+    const domain = process.env.NEXT_PUBLIC_API_DOMAIN;
+    // get cookies from browser req to be used in server component
+    const cookieStore = await cookies();
+    const cookieHeader = cookieStore.toString();
+
+    const response = await fetch(`${domain}/dashboard`, {
+        headers: {
+            Cookie: cookieHeader, // send cookies with request
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        cache: "no-store",
+    });
+
+    const data = await response.json();
+    if (!response.ok) console.error(data)
 
     const now = new Date();
-    const currentDate = now.toLocaleDateString(); // 'DD/MM/YYYY'
-    const today = now.getDate(); // 26
-    const todayName = now.toLocaleDateString("en-US", { weekday: "long", }); // ltniin hhhh 
-
     const thisMonth = now.getMonth();
     const thisYear = now.getFullYear();
 
-    const daysInMonths = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-    let current_log_index = 0
-    // fill the days in month array by days in month as objects
+    const daysInThisMonths = new Date(thisYear, thisMonth + 1, 0).getDate();
     const daysInMonth = [];
-    for (let i = 1; i <= daysInMonths; i++) daysInMonth.push(new Date(thisYear, thisMonth, i));
-
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await fetch(`${domain}/dashboard`, {
-                    credentials: "include", // send cookies with request
-                    headers: {
-                        "Accept": "application/json",
-                        "Content-Type": "application/json"
-                    }
-                });
-
-                const result = await response.json();
-                if (response.ok) {
-                    console.log("result : ", result);
-                    setData(result);
-                }
-
-            } catch (error) {
-                console.error(error);
-            }
-        })()
-    }, []);
-
-
+    for (let i = 1; i <= daysInThisMonths; i++) daysInMonth.push(new Date(thisYear, thisMonth, i));
 
     return (
         <section className="space-y-4">
@@ -77,38 +58,16 @@ export default function board() {
                                     </thead>
                                     <tbody>
 
-                                        {data?.habits.map((h, key) =>
-
+                                        {data?.habits.map((habit, key) =>
+                                            // show every habits log row by row .
                                             <tr key={key} className="border border-[#86878b] bg-[#0d1117] hover:bg-[#23262d] hover:border-blue-400">
-                                                <td className="px-3 min-w-[25px] bg-inherit sticky left-[0px] block m-0 border-x"> <a href="{{ route('habits.show' , $h->id) }}">{h.title}</a></td>
-                                                {daysInMonth.map((i, key) =>
-                                                    <>
-                                                        {(currentDate > i.toLocaleDateString() && h.frequency.includes(todayName) && new Date(h.created_at).toLocaleDateString() <= i.toLocaleDateString()) ?
-
-                                                            (h.logs[current_log_index] && new Date(h.logs[current_log_index].completed_date).toLocaleDateString() == i.toLocaleDateString()) ?
-
-                                                                <td key={key} className="border-x border-solid border-[#86878b] min-w-[25px] text-center bg-green-800">
-                                                                    {current_log_index++}
-                                                                    <img src="/svg/ok.svg" className="w-[15px] m-auto" alt="" />
-                                                                </td>
-                                                                :
-                                                                <td key={key} className="border-x border-solid border-[#86878b] min-w-[25px] text-center bg-red-800">
-                                                                    <img src="/svg/x.svg" className="w-[15px] m-auto" alt="" />
-                                                                </td>
-
-                                                            : (currentDate == i.toLocaleDateString() && h.frequency.includes(todayName))
-                                                                ?
-                                                                <Log habit={h} index={i} key={key} />
-                                                                :
-                                                                <td className="border-x border-solid border-[#86878b] min-w-[25px] text-center"></td>
-                                                        }
-                                                        {i == daysInMonth[-1] ? current_log_index = 0 : ''}
-
-                                                    </>
-                                                )}
-
+                                                <td className="px-3 min-w-[25px] bg-inherit sticky left-[0px] block m-0 border-x">
+                                                    <a href="{{ route('habits.show' , $h->id) }}">{habit.title}</a>
+                                                </td>
+                                                <HabitRow habit={habit} daysInMonth={daysInMonth} />
                                             </tr>
                                         )}
+
                                     </tbody>
                                 </table>
                             </div>
