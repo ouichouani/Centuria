96f62a96753b70c05717b55a2c7e9dc8ac2e97ab
ouichouani <ouichouaniabdlhakim@gmail.com>
2026-06-22 18:40:42 +0100

    add the page requirement in some pages

--- frontend_Centuria/app/main/controll-panel/layout.jsx
+++ frontend_Centuria/app/main/controll-panel/layout.jsx
@@ -9,19 +9,17 @@ export default function ControllPanelLayout({ children }) {
     const { setNav } = useContext(NavContext);
 
     useEffect(() => {
-        setNav(
-            <>
-                <Link href={'/main/controll-panel/blacklist'}>black list</Link>
-                <Link href={'/main/controll-panel/posts'}>posts</Link>
-                <Link href={'/main/controll-panel/reports'}>reports</Link>
-            </>
-        )
+        setNav([
+            { title: 'blacklist', href: '/main/controll-panel/blacklist', imageSrc: '/svg/notification.svg' },
+            { title: 'hidden-posts', href: '/main/controll-panel/posts', imageSrc: '/svg/notification.svg' },
+            { title: 'reports', href: '/main/controll-panel/reports', imageSrc: '/svg/notification.svg' },
+        ]);
     }, []);
 
 
     return (
         <>
-        { children }
+            {children}
         </>
     )
 
